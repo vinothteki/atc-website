1c7d200c836aa9ca0117dda5c019d8294ba7f65e
vinothki <vinoth@auzmor.com>
2018-04-12 13:37:30 +0530

    design change

---
+++ carousel.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        items: 1,
+        /*responsive:{
+            0:{
+                items:1
+            }
+        }*/
+    });
+});
+// 1290, 350
